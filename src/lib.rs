f742ca7cee4ce22760c7c26e5d4461dbb3794759
Jonghyun Lim <limjonghyun@gmail.com>
2022-03-25 15:38:15 +0900

    Add subscription handler

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
+use std::net::TcpListener;
+
 use actix_web::dev::Server;
 use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer, Responder};
-use std::net::TcpListener;
 
 pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
     Ok(HttpServer::new(|| {
@@ -8,6 +9,7 @@ pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
             .route("/", web::get().to(greet))
             .route("/{name}", web::get().to(greet))
             .route("/health_check", web::get().to(health_check))
+            .route("/subscriptions", web::post().to(subscribe))
     })
     .listen(listener)?
     .run())
@@ -19,5 +21,15 @@ async fn greet(req: HttpRequest) -> impl Responder {
 }
 
 async fn health_check() -> impl Responder {
+    HttpResponse::Ok().finish()
+}
+
+#[derive(serde::Deserialize)]
+pub struct FormData {
+    email: String,
+    name: String,
+}
+
+async fn subscribe(_form: web::Form<FormData>) -> HttpResponse {
     HttpResponse::Ok().finish()
 }
